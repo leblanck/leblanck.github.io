ddf78853c9ad2803dc0f148e3a128d3587b12fed
Kyle LeBlanc <kyle.paul.leblanc@gmail.com>
2018-05-02 16:56:20 -0400

    init commit
    init commit2

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(funciton() {
+  const d = new Date();
+  const hours = d.getHours();
+  const night = hours >= 19 || hours <= 7; //inbetween 7pm and 7am
+  const body = document.querySelector('body');
+  const toggle = document.getElementById('toggle');
+  const input = document.getElementById('switch');
+
+  if (night) {
+    input.checked = true;
+    body.classList.add('night');
+  }
+
+  toggle.addEventListener('click', funciton() {
+    const isChecked = input.checked;
+    if (isChecked) {
+      body.classList.remove('night');
+    } else {
+      body.classList.add('night');
+    }
+  });
+
+  const introHeight = document.querySelector('.intro').offsetHeight;
+  const topButton = document.getElementById('top-button');
+  const $topButton = $('#top-button');
+
+  window.addEventListener(
+    'scroll'
+    function()
+  )
+
+
+
+
+
+
+
+});
