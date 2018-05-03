c4d00dd58f4f0b9742304e3ffd0c9139e061b8ef
Kyle LeBlanc <kyle.paul.leblanc@gmail.com>
2018-05-03 16:27:03 -0400

    home page
    created index.html

--- js/main.js
+++ js/main.js
@@ -26,13 +26,51 @@ $(funciton() {
 
   window.addEventListener(
     'scroll'
-    function()
-  )
+    function() {
+      if (window.scrollY > introHeight) {
+        $topButton.fadeIn();
+      } else {
+        $topButton.fadeOut();
+      }
+    },
+    false
+  );
 
+topButton.addEventListener('click', function() {
+  $('html, body').animate({ scrollTop: 0 }, 500);
+});
 
+const hand = document.querySelector('.emoji.wave-hand');
 
+function waveOnLoad() {
+  hand.classList.add('wave');
+  setTimeout(function() {
+    hand.classList.remove('wave');
+  }, 2000);
+}
 
+setTimeout(function () {
+  waveOnLoad();
+}, 1000);
 
+hand.addEventListener('mouseover', function() {
+  hand.classList.add('wave');
+});
 
+hand.addEventListener('mouseout', function() {
+  hand.classList.remove('wave');
+});
 
+window.sr = scrollreveal({
+  reset: false,
+  duration: 600;
+  easing: 'cubic-bezier(.694,0,.355,1)',
+  viewFactor: 0.3,
+});
+
+sr.reveal('.background');
+sr.reveal('.skills');
+sr.reveal('.experience', { viewFactor: 0.2 });
+sr.reveal('.featured-projects', { viewFactor: 0.1 });
+sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
