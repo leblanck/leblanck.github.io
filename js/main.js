d2c8b03a68138661ef10a439830419afcbbcc651
Kyle LeBlanc <kyle.paul.leblanc@gmail.com>
2018-05-07 16:01:41 -0400

    wow.js
    added wow.js for section fade ins

--- js/main.js
+++ js/main.js
@@ -61,17 +61,17 @@ $(function() {
     hand.classList.remove('wave');
   });
 
-  window.sr = ScrollReveal({
-    reset: false,
-    duration: 600,
-    easing: 'cubic-bezier(.694,0,.335,1)',
-    scale: 1,
-    viewFactor: 0.3,
-  });
+  //window.sr = ScrollReveal({
+  //  reset: false,
+  //  duration: 600,
+  //  easing: 'cubic-bezier(.694,0,.335,1)',
+  //  scale: 1,
+  //  viewFactor: 0.3,
+  //});
 
-  sr.reveal('.background');
-  sr.reveal('.skills');
-  sr.reveal('.experience', { viewFactor: 0.2 });
-  sr.reveal('.featured-projects', { viewFactor: 0.1 });
-  sr.reveal('.other-projects', { viewFactor: 0.05 });
+  //sr.reveal('.background');
+  //sr.reveal('.skills');
+  //sr.reveal('.experience', { viewFactor: 0.2 });
+  //sr.reveal('.featured-projects', { viewFactor: 0.1 });
+  //sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
